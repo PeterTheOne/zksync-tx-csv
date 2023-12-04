c41f27ee0df9d868866c4f2631eb5ac96e637968
PeterTheOne <petertheone@gmail.com>
2023-12-04 16:48:29 +0100

    also add token info

--- index.js
+++ index.js
@@ -1,26 +1,83 @@
 import fetch from "node-fetch";
 import fs from 'fs';
 
-async function start() {
-  const account = '0x9fc13b4e1c4206970a1c4520d2f77336cd5d0e0a';
+const TOKEN_LIMIT = 100;
+const TX_LIMIT = 100;
+const API_BASE_URL = 'https://api.zksync.io/api/v0.2';
 
+// todo: move to dotenv or console args
+const account = '0x9fc13b4e1c4206970a1c4520d2f77336cd5d0e0a';
+
+async function fetchTokens() {
+  let tokens = {};
+  let from = 'latest';
+  let tokenCount = 0;
+  let seenTokenIds = new Set(); // Set to track seen tokens
+
+  while (true) {
+    console.log(`Fetching tokens starting from ID: ${from}`);
+    const response = await fetch(`${API_BASE_URL}/tokens?from=${from}&limit=${TOKEN_LIMIT}&direction=older`);
+    const { status, result } = await response.json();
+    if (status !== 'success' || result.list.length === 0) {
+      break;
+    }
+    result.list.forEach(token => {
+      if (!seenTokenIds.has(token.id)) {
+        tokens[token.id] = token;
+        tokenCount++;
+        seenTokenIds.add(token.id);
+      }
+    });
+    console.log(`Fetched ${tokenCount} tokens so far`);
+    
+    if (result.list.length < TOKEN_LIMIT) {
+      break;
+    }
+
+    from = result.list[result.list.length - 1].id;
+  }
+  console.log(`Finished fetching tokens. Total tokens fetched: ${tokenCount}`);
+  return tokens;
+}
+
+async function fetchTransactions(account, tokens) {
   let from = 'latest';
   let txList = [];
-  let success = true;
-  do {
-    console.log('fetch from', from);
-    const result = await fetch(`https://api.zksync.io/api/v0.2/accounts/${account}/transactions?from=${from}&limit=100&direction=older`);
-    const resultJson = await result.json();
-    success = resultJson.status === 'success';
-    if (success) {
-      from = resultJson.result.list[resultJson.result.list.length - 1].txHash;
-      txList = [...txList.slice(0, txList.length - 1), ...resultJson.result.list];
-      console.log(`${txList.length} from ${resultJson.result.pagination.count}`);
-      if (resultJson.result.list.length === 1) {
-        success = false;
-      }
+  let transactionCount = 0;
+  let seenTxHashes = new Set(); // Set to track seen transactions
+
+  while (true) {
+    console.log(`Fetching transactions starting from hash: ${from}`);
+    const response = await fetch(`${API_BASE_URL}/accounts/${account}/transactions?from=${from}&limit=${TX_LIMIT}&direction=older`);
+    const { status, result } = await response.json();
+    if (status !== 'success' || result.list.length === 0) {
+      break;
     }
-  } while (success);
+    result.list.forEach(tx => {
+      if (!seenTxHashes.has(tx.txHash)) {
+        if (tx.op && tokens[tx.op.token]) {
+          tx.op.tokenDetails = tokens[tx.op.token];
+        }
+        txList.push(tx);
+        transactionCount++;
+        seenTxHashes.add(tx.txHash);
+      }
+    });
+    console.log(`Fetched ${transactionCount} transactions so far`);
+
+    if (result.list.length < TX_LIMIT) {
+      break;
+    }
+
+    from = result.list[result.list.length - 1].txHash;
+  }
+  console.log(`Finished fetching transactions. Total transactions fetched: ${transactionCount}`);
+  return txList;
+}
+
+async function start() {
+  const tokens = await fetchTokens();
+  const txList = await fetchTransactions(account, tokens);
 
   // Convert to CSV
   const csv = jsonToCSV(txList);

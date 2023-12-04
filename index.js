f6014c14d9728b14dd3cccff73d0917a21cd9a0d
PeterTheOne <petertheone@gmail.com>
2023-12-04 16:14:20 +0100

    create csv file

--- index.js
+++ index.js
@@ -20,8 +20,48 @@ async function start() {
         success = false;
       }
     }
-    fs.writeFileSync('result.json', JSON.stringify(txList));
   } while (success);
+
+  // Convert to CSV
+  const csv = jsonToCSV(txList);
+  fs.writeFileSync('result.csv', csv);
+}
+
+function flattenObject(obj, parentPrefix = '') {
+  let flattened = {};
+
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      const flatChild = flattenObject(obj[key], `${parentPrefix}${key}.`);
+      Object.assign(flattened, flatChild);
+    } else {
+      flattened[`${parentPrefix}${key}`] = obj[key];
+    }
+  }
+
+  return flattened;
+}
+
+function jsonToCSV(jsonArray) {
+  if (jsonArray.length === 0) {
+    return '';
+  }
+
+  // Flatten each object in the array
+  const flattenedArray = jsonArray.map(item => flattenObject(item));
+
+  // Extract headers
+  const headers = Object.keys(flattenedArray[0]);
+  const csvRows = flattenedArray.map(row => 
+    headers.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(',')
+  );
+
+  return [headers.join(','), ...csvRows].join('\r\n');
+
+  function replacer(key, value) {
+    if (value == null) return '';
+    return value;
+  }
 }
 
 start();
